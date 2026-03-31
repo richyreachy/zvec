04d86ff0f417a9075644a260aed304cce8bd6b5f
ray <rui.xing@alibaba-inc.com>
2026-03-31 14:45:52 +0800

    feat: add scalar dist funcs

---
+++ src/turbo/scalar/float16/cosine.cc
@@ -0,0 +1,34 @@
+// Copyright 2025-present the zvec project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "scalar/float16/cosine.h"
+#include "scalar/float16/inner_product.h"
+
+namespace zvec::turbo::scalar {
+
+void cosine_fp16_distance(const void *a, const void *b, size_t dim,
+                          float *distance) {
+  constexpr size_t extra_dim = 2;
+  size_t original_dim = dim - extra_dim;
+
+  float ip;
+  inner_product_fp16_distance(a, b, original_dim, &ip);
+
+  *distance = 1 - ip;
+}
+
+void cosine_fp16_batch_distance(const void *const *vectors, const void *query,
+                                size_t n, size_t dim, float *distances) {}
+
+}  // namespace zvec::turbo::scalar
